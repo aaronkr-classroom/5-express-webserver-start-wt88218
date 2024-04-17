1c5dd27a2ec6f52358307b5172b717c5f6b6d9c2
wt88218 <163230869+wt88218@users.noreply.github.com>
2024-04-17 05:15:18 +0000

    11끝

--- lesson-11/controllers/homeController.js
+++ lesson-11/controllers/homeController.js
@@ -4,14 +4,37 @@
 /**
  * Listing 10.2
  */
-exports.respondWithName = (req, res) => {
+exports.respondWithName1 = (req, res) => {
+
     res.render('index');
 }
 
 /**
  * Listing 10.3
  */
-exports.respondWithName2 = (req, res) => {
-    let paramsName = req.params.myName;
-    res.render('index', { name: paramsName });
+// exports.getHome = (req, res) => { res.render('index'); } // "/"index
+// exports.getContact = (req, res) => {} // "/"contact Get method
+// exports.postContact = (req, res) => {} // "/"contact Post method
+
+exports.respondWithName = (req, res) => {
+    console.log(req.params);
+    let paramsName = req.params.myName; // 라우트의 매개변수 /name/:myName
+    res.render('index', { 
+        name: paramsName,
+        people:{
+            usa:{
+                einstein: "Albert Einstein",
+                musk: "Elon Musk",
+                jobs: "Steve Jobs"
+            },
+            Korea: {
+                psy: "싸이다!",
+                kim: "김예수",
+            },
+            mexico: {
+                jesus: "jesus Alvarez",
+                
+            }
+        }
+        }); //객체전송
 };
--- lesson-11/main.js
+++ lesson-11/main.js
@@ -1,4 +1,38 @@
 // main.js
 "use strict";
-
+// BEST 습관 : 먼저 모든 상수나 변수 정의(const)
 // @TODO: 웹 서버를 만들고 실행한다.
+const port = 3000,
+    express = require('express'),
+    layouts = require('express-ejs-layouts'),
+    homeController = require('./controllers/homeController'),
+    errorController = require('./controllers/errorController'),
+    app = express();
+
+    //BEST 습관 : 2단계 앱 설정( set 함수, 그다음에 use 함수)
+app.set("port", process.env.PORT || port);
+app.set("view engine", "ejs");
+
+app.use(layouts);
+app.use(express.static("public"));
+
+
+
+//Aaron 습관 : GET 라우트 먼저 그 다음에 POST 라우트
+// app.get("/", homeController.getHome);
+app.get("/name/:myName", homeController.respondWithName);
+app.get("/test", (req, res) =>{
+    res.sendFile("./public/test.html");
+});
+
+// errorController app.use() , 순서때문에 errorController app.use는 거의 마지막 (listen전!)
+app.use(errorController.logErrors);
+app.use(errorController.resNotFound); // 404
+app.use(errorController.resInternalError); // 500 , 500에러는 항상 마지막 (Last error)
+
+    //BEST 습관 : 마지막 listen 함수
+
+app.listen(app.get("port"), () => {
+    console.log(`server at: http://localhost:${app.get("port")}`);
+
+});
